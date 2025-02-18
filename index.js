d071e2ec7623c25a5ff596636b8f39f4ff9dbdec
Your name <your-email@example.com>
2025-02-18 23:28:31 +0530

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-console.log("This is string tutorial")
+// console.log("This is string tutorial")
 
-let a="harry"
-console.log(a[0])
-console.log(a[1])
-console.log(a[2])
-console.log(a[3])
-console.log(a[4])
+// let a="harry"
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
 
-console.log(a.length)
+// console.log(a.length)
 
-let real_name = "harry"
-let friends = "Rohan"
-console.log("The real name is " + real_name + " and Friends name is " +friends) 
-console.log(`The real name is ${real_name} the friends name is ${friends}`)
+// let real_name = "harry"
+// let friends = "Rohan"
+// console.log("The real name is " + real_name + " and Friends name is " +friends) 
+// console.log(`The real name is ${real_name} the friends name is ${friends}`)
 
-let b= "shivam"
-console.log(b.toUpperCase())
-console.log(b.slice(2,4))
-console.log(b.replace("sh","boka"))
+// let b= "shivam"
+// console.log(b.toUpperCase())
+// console.log(b.slice(2,4))
+// console.log(b.replace("sh","boka"))
+
+let a="hello world"
+console.log(a)
+console.log("har\"".length)
+console.log(a.startsWith("he"))
+console.log(a.endsWith("l"))
+console.log(a.toLowerCase())
+let str="Please give Rs 1000"
+console.log(str)
+console.log(str.includes(" give "))
+console.log(str.slice(15))
+
