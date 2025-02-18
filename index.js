885347d9a2a125c747cf29331237a4b7be49ee7d
Your name <your-email@example.com>
2025-02-18 22:48:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("This is string tutorial")
+
+let a="harry"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let real_name = "harry"
+let friends = "Rohan"
+console.log("The real name is " + real_name + " and Friends name is " +friends) 
+console.log(`The real name is ${real_name} the friends name is ${friends}`)
+
+let b= "shivam"
+console.log(b.toUpperCase())
+console.log(b.slice(2,4))
+console.log(b.replace("sh","boka"))
